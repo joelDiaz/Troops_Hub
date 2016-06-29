446458e1c95f417850af41515c0f54eeb606fc0a
adrianjp2 <adrianperez1223@gmail.com>
2016-06-29 09:39:32 -0430

    primera subida

---
+++ app/src/main/java/com/example/joel/troopshub/start_app.java
@@ -0,0 +1,24 @@
+package com.example.joel.troopshub;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class start_app extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_app);
+
+        new CountDownTimer(1000, 1000) {
+            public void onTick(long millisUntilFinished) {
+            }
+            public void onFinish() {
+//                Intent intent = new Intent(general.this, ventana2.class);
+//                startActivity(intent);
+            }
+        }.start();
+    }
+}
