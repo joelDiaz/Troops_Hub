b12bbc29e56b99f10c2b5fa5e57e69d32b040b3c
Moririn27 <leoflash21@gmail.com>
2016-06-30 17:29:17 -0430

    Add new page

--- app/src/main/java/com/example/joel/troopshub/Categories.java
+++ app/src/main/java/com/example/joel/troopshub/Categories.java
@@ -1,6 +1,10 @@
 package com.example.joel.troopshub;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +20,12 @@ public class Categories extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.categories,container,false);
+
+        Toolbar toolbar = (Toolbar)v.findViewById(R.id.appbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar( toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
         return v;
     }
 }
